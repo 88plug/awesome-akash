bfacd5c207dc9c878894fbbc3b7cefb28bbc0b8b
Andrew Mello <andrew@88plug.com>
2024-08-19 21:18:25 -0700

    More MO updates

--- moneroocean/run.sh
+++ moneroocean/run.sh
@@ -25,21 +25,22 @@ echo "Found $MEMORY_SIZE of memory available."
 
 if [ -z $WALLET ]; then
     echo "Please examine the SDL and be sure to set your Monero Wallet Address in the WALLET= variable."
-    sleep 10
+    sleep 5
     exit
 fi
 
 if [ -z $MODE ]; then
     echo "Please examine the SDL and be sure to set the mode to fast or light in the MODE= variable."
-    sleep 10
+    sleep 5
     exit 1
 fi
 
 if [ -z $MODE ]; then
     echo "Please examine the SDL and be sure to set the mode to fast or light in the MODE= variable."
-    sleep 10
+    sleep 5
     exit 1
 fi
+
 
 echo "Checking for NUMA nodes"
 NUMA_NODES=$(lscpu | grep "NUMA node(s)" | head -n1 | awk '{print $3}')
@@ -54,48 +55,47 @@ check_memory() {
         echo -e "###      You must close this deployment to change the       ###"
         echo -e "###      memory requested.                                  ###"
         echo -e "############################################################\n"
-        sleep 10
+        sleep 5
         exit 1
     elif (( $(echo "$MEMORY_SIZE < $required_memory" | bc -l) )); then
         echo -e "\n############################################################"
         echo -e "###      ERROR: You do not have enough memory allocated   ###"
-        echo -e "###      for the NUMA nodes. Increase the requested       ###"
+        echo -e "###      for this deployment. Increase the requested      ###"
         echo -e "###      memory for this deployment to >= ${required_memory}Gi.    ###"
         echo -e "###      You must close this deployment to change the     ###"
         echo -e "###      memory requested.                                ###"
         echo -e "############################################################\n"
-        sleep 10
+        sleep 5
         exit 1
-    elif (( $(echo "$MEMORY_SIZE > $required_memory" | bc -l) )); then
-        echo -e "\n############################################################"
-        echo -e "###      WARNING: You have allocated more memory than     ###"
-        echo -e "###      required for the NUMA nodes! Consider adjusting  ###"
-        echo -e "###      the memory to optimize resource usage.           ###"
-        echo -e "############################################################\n"
     else
         echo "Found enough memory!"
     fi
 }
 
-if [[ $NUMA_NODES -gt 1 && $MODE == fast ]]; then
-    echo "Detected $NUMA_NODES NUMA nodes on this provider! Ensuring you have set enough memory..."
-
+if [[ $NUMA_NODES -gt 1 && "$MODE" == "fast" ]]; then
+    echo "Detected $NUMA_NODES NUMA nodes on this provider! Checking if you have set enough memory..."
     case $NUMA_NODES in
-        2) check_memory 4.75 ;;
-        3) check_memory 6.5 ;;
-        4) check_memory 9.5 ;;
-        *) check_memory 3 ;;
+        1) required_memory=3 ;;
+        2) required_memory=4.75 ;;
+        3) required_memory=6.5 ;;
+        4) required_memory=9.5 ;;
+        8) required_memory=16 ;;
+        *) required_memory=3 ;;
     esac
-
-elif [[ $MODE == light ]]; then
-    echo "Mode is set to light. Ensuring at least 1 GiB of memory is allocated..."
-    check_memory 1
+elif [[ $MODE == "light" ]]; then
+    echo "Mode is set to light. Checking if at least 1 GiB of memory is allocated..."
+    required_memory=1
 else
-    echo "Found 1 NUMA node or MODE is not fast. No additional NUMA-related memory checks required."
-    if [[ $MODE == fast ]]; then
-        check_memory 3
+    echo "Found 1 NUMA node or MODE is not fast. Checking for standard memory requirements..."
+    if [[ $MODE == "fast" ]]; then
+        required_memory=3
+    else
+        required_memory=1
     fi
 fi
+
+echo "Checking if allocated memory ($MEMORY_SIZE Gi) meets the requirement of $required_memory Gi"
+check_memory $required_memory
 
 
 
@@ -119,7 +119,6 @@ sed -i 's/"cn\/0": *[^,]*,/"cn\/0": true,/' /root/moneroocean/config.json
 sed -i 's/"cn\-lite\/0": *[^,]*,/"cn-lite\/0": true,/' /root/moneroocean/config.json
 sed -i 's/"astrobwt\-avx2": *[^,]*,/"astrobwt-avx2": true,/' /root/moneroocean/config.json
 
-#sed -i 's/"hw-aes": *[^,]*,/"hw-aes": false,/' /root/moneroocean/config.json
 sed -i 's/"yield": *[^,]*,/"yield": false,/' /root/moneroocean/config.json
 sed -i 's/"wrmsr": *[^,]*,/"wrmsr": -1,/' /root/moneroocean/config.json
 sed -i 's/"rdmsr": *[^,]*,/"rdmsr": -1,/' /root/moneroocean/config.json
@@ -132,67 +131,70 @@ sed -i 's/"donate-level": *[^,]*,/"donate-level": 0,/' /root/moneroocean/config.
 sed -i 's/"donate-over-proxy": *[^,]*,/"donate-over-proxy": 0,/' /root/moneroocean/config.json
 sed -i 's/"pass": *[^,]*,/"pass": "'"${WORKER}-${AKASH_CLUSTER_PUBLIC_HOSTNAME}"'",/' /root/moneroocean/config.json
 sed -i 's/"user": *[^,]*,/"user": "'"$WALLET"'",/' /root/moneroocean/config.json
-#sed -i 's/"url": *[^,]*,/"url": "gulf.moneroocean.stream:20128",/' /root/moneroocean/config.json
 sed -i '/"tls": {/!b;n;s/"enabled": false/"enabled": true/' /root/moneroocean/config.json
-#cat /root/moneroocean/config.json
 
 # Define the path to your configuration file
 CONFIG_FILE="/root/moneroocean/config.json"
 
-
-# Function to check for a CPU feature using lscpu
 check_cpu_feature() {
-    feature=$(lscpu | grep "$1")
-    if [ -n "$feature" ]; then
-        echo "Found $1 support."
+    feature="$1"  # Accept the feature as an argument
+    flags=$(lscpu -J | jq -r '.lscpu[] | select(.field == "Flags:").data')
+    if [[ $flags == *"$feature"* ]]; then
+        echo "Found $feature support."
         return 0
     else
-        echo "$1 support not found."
+        echo "$feature support not found."
         return 1
     fi
 }
 
 # Set huge-pages based on the HUGE_PAGES variable
-if [[ $HUGE_PAGES == true ]]; then
+if [[ "$HUGE_PAGES" == "true" && "$MODE" == "fast" ]]; then
     sed -i 's/"huge-pages": *[^,]*,/"huge-pages": true,/' $CONFIG_FILE
     sed -i 's/"huge-pages-jit": *[^,]*,/"huge-pages-jit": true,/' $CONFIG_FILE
+    echo "Huge Pages Enabled"
 else
     sed -i 's/"huge-pages": *[^,]*,/"huge-pages": false,/' $CONFIG_FILE
     sed -i 's/"huge-pages-jit": *[^,]*,/"huge-pages-jit": false,/' $CONFIG_FILE
+    echo "Huge Pages Disabled"
 fi
 
 # Set 1gb-pages based on the GB_PAGES variable
-if [[ $GB_PAGES == true ]]; then
+if [[ "$GB_PAGES" == "true" && "$MODE" == "fast" ]]; then
     sed -i 's/"1gb-pages": *[^,]*,/"1gb-pages": true,/' $CONFIG_FILE
+    echo "GB Pages Enabled"
 else
     sed -i 's/"1gb-pages": *[^,]*,/"1gb-pages": false,/' $CONFIG_FILE
+    echo "GB Pages Disabled"
+fi
+
+# Always set init-avx2 to true for performance
+if check_cpu_feature 'avx2'; then
+    sed -i 's/"init-avx2": *[^,]*,/"init-avx2": true,/' $CONFIG_FILE
+else
+    echo "AVX2 Support not found"
+    sed -i 's/"init-avx2": *[^,]*,/"init-avx2": false,/' $CONFIG_FILE
 fi
 
 # Always set hw-aes to true for performance
-if check_cpu_feature 'aes'; then
-    sed -i 's/"hw-aes": *[^,]*,/"hw-aes": true,/' $CONFIG_FILE
-
-else
-    echo "HW-AES Support not found"
-    sed -i 's/"hw-aes": *[^,]*,/"hw-aes": false,/' $CONFIG_FILE
-fi
+#if check_cpu_feature 'aes'; then
+#    sed -i 's/"hw-aes": *[^,]*,/"hw-aes": true,/' $CONFIG_FILE
+#
+#else
+#    echo "AES Support not found"
+#    sed -i 's/"hw-aes": *[^,]*,/"hw-aes": false,/' $CONFIG_FILE
+#fi
 
 # Set CPU priority to highest (0) for max performance
 sed -i 's/"priority": *[^,]*,/"priority": 0,/' $CONFIG_FILE
 
-# Set argon2-impl based on AVX2 support
-if check_cpu_feature 'avx2'; then
-echo "Set argon2-impl based on AVX2 support"
-    sed -i 's/"argon2-impl": *[^,]*,/"argon2-impl": "AVX2",/' $CONFIG_FILE
-    sed -i 's/"init-avx2": *[^,]*,/"init-avx2": 1,/' $CONFIG_FILE
-else
-    echo "AVX2 not found, defaulting to SSE2 for argon2-impl."
-    sed -i 's/"argon2-impl": *[^,]*,/"argon2-impl": "SSE2",/' $CONFIG_FILE
-    sed -i 's/"init-avx2": *[^,]*,/"init-avx2": 0,/' $CONFIG_FILE
-fi
-
-echo "Configuration updated for maximum performance based on CPU."
-
-
+echo
+echo "╔═══════════════════════════════════════════════════════════════════════════════════════════════════╗"
+echo "║                                                                                                   ║"
+echo "║   Configuration optimized by Crypto and Coffee for max performance in Kubernetes.                 ║"
+echo "║   https://cryptoandcoffee.com                                                                     ║"
+echo "║                                                                                                   ║"
+echo "╚═══════════════════════════════════════════════════════════════════════════════════════════════════╝"
+echo
 
 /root/moneroocean/miner.sh --config=/root/moneroocean/config.json
